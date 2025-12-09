46597920a79342386ccf518996468be8cfbe7491
ViosGradeRaise <gv77230@gmail.com>
2025-12-10 01:21:02 +0300

    Create binaryheap.java

---
+++ binaryheap.java
@@ -0,0 +1,19 @@
+public class BinaryHeapExample {
+    public static void main(String[] args) {
+        // Бинарная куча через PriorityQueue
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        heap.add(50);
+        heap.add(20);
+        heap.add(70);
+        heap.add(10);
+        heap.add(90);
+
+        // Вывести и удалить элементы в куче в порядке возрастания
+        System.out.println("Elements in heap order:");
+        while (!heap.isEmpty()) {
+            System.out.print(heap.poll() + " ");
+        }
+        System.out.println();
+    }
+}
